5da4b34056754b58d0126efe41b1a243f36413f3
Balbir singh <bsingharora@gmail.com>
2019-12-04 04:13:29 +1100

    Initial commit
    Add basic go parser, see [TODOs in README]
    
    Signed-off-by: Balbir singh <bsingharora@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # gocve
 A Parser for parsing NIST CVEs in golang
+
+It's still very very much WIP, does some basic printing of descriptions.
+To test download and unzip the feed from https://nvd.nist.gov/vuln/data-feeds
+
+## TODOs
+1. Implement cpe matching
+1. Implement impact based filtering
+1. Implement http loading of the content and decoding the content
+
+## NOTE
+I am using this as a tool to teach me golang, feel free to criticize
+the code.
---
+++ cve.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type CVEDescriptionData struct {
+	Lang  string `json:"lang"`
+	Value string `json:"value"`
+}
+
+type CVEDescription struct {
+	Description []*CVEDescriptionData `json:"description_data"`
+}
+
+type CVE struct {
+	Description  CVEDescription `json:"description"`
+	data_type    string
+	data_format  string
+	data_version string
+	meta_data    string
+	problem_type string
+	references   string
+}
+
+type CVENodes struct {
+	CVEOperator string         `json:"operator,omitempty"`
+	CVENegates  string         `json:"negates,omitempty"`
+	CPEMatch    []*CVECPEMatch `json:"cpe_match,omitempty"`
+	CVEChildren []*CVENodes    `json:"children,omitempty"`
+}
+
+type CVECPEName struct {
+	CPE23Uri         string `json:"cpe23Uri"`
+	CPE22Uri         string `json:"cpe22Uri,omitempty"`
+	LastModifiedDate string
+}
+
+type CVECPEMatch struct {
+	Vulnerable            bool   `json:"vulnerable"`
+	CPE23Uri              string `json:"cpe23Uri"`
+	CPE22Uri              string `json:"cpe22Uri,omitempty"`
+	VersionStartExcluding string
+	VersionStartIncluding string
+	VersionEndExcluding   string
+	VersionEndIncluding   string
+	CVECPEName            []*CVECPEName `json:"cpe_name,omitempty"`
+}
+
+type CVEItemConfiguration struct {
+	CVEDataVersion string      `json:"CVE_data_version"`
+	CVENode        []*CVENodes `json:"nodes,omitempty"`
+}
+
+type CVEBaseMetricV2 struct {
+}
+
+type CVEBaseMetricV3 struct {
+}
+
+type CVEImpact struct {
+	BaseMetricV3 CVEBaseMetricV3
+	BaseMetricV2 CVEBaseMetricV2
+}
+
+type CVEItem struct {
+	CVEInfo          CVE                  `json:"cve"`
+	Configuration    CVEItemConfiguration `json:"configurations,omitempty"`
+	Impact           CVEImpact            `json:"impact,omitempty"`
+	PublishedDate    string               `json:"publishedDate,omitempty"`
+	LastModifiedDate string               `json:"lastModifiedDate,omitempty"`
+}
+
+type CVEMain struct {
+	CVEDataType    string
+	CVEDataFormat  string
+	CVEDataVersion string
+	CVENumber      int
+	CVETimestamp   string
+	CVEItems       []*CVEItem `json:"CVE_Items"`
+}
+
+func main() {
+	file, err := os.Open("nvdcve-1.1-modified.json")
+	if err != nil {
+		log.Fatal("Failed to open JSON file with CVEs")
+	}
+
+	var result CVEMain
+
+	if err := json.NewDecoder(file).Decode(&result); err != nil {
+		fmt.Printf("%s\n", err)
+		log.Fatal("Failed to parse JSON file")
+	}
+
+	//fmt.Printf("%v", result.CVEItems)
+	for i, item := range result.CVEItems {
+		for _, desc := range item.CVEInfo.Description.Description {
+			fmt.Printf("%v:%v\n", i, desc.Value)
+		}
+	}
+}
